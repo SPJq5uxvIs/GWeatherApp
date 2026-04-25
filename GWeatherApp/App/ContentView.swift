4f62ff19707b2b4a802a30d1a4075d5d28bffd90
Spenzer Reyes <s.reyes@betrnk-tours.com>
2026-04-25 11:41:45 +0800

    Initial commit

---
+++ GWeatherApp/App/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  GWeatherApp
+//
+//  Created by IOS-Dev on 4/25/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GWeatherApp/App/GWeatherAppApp.swift
@@ -0,0 +1,18 @@
+//
+//  GWeatherAppApp.swift
+//  GWeatherApp
+//
+//  Created by IOS-Dev on 4/25/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct GWeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
