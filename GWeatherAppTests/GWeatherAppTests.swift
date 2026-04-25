0bf62108d243f26e1276c6f826ff312c4cd73cc6
Spenzer Reyes <s.reyes@betrnk-tours.com>
2026-04-25 11:40:19 +0800

    Initial Commit

---
+++ GWeatherAppTests/GWeatherAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  GWeatherAppTests.swift
+//  GWeatherAppTests
+//
+//  Created by IOS-Dev on 4/25/26.
+//
+
+import Testing
+@testable import GWeatherApp
+
+struct GWeatherAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
